d774c32a0798ad99abacaf6254b03d624652eed4
ZetaDaemon <zetademon@icloud.com>
2026-08-09 21:48:13 +1000

    build_mod

--- python/bpd_vars/__init__.py
+++ python/bpd_vars/__init__.py
@@ -1,6 +1,9 @@
-from . import _bpd_vars_native
+from mods_base import Library, build_mod
+
 from .behavior_variable import BehaviorVariable
+from .commands import update_variable
 
 __all__ = ["BehaviorVariable"]
 
-print(_bpd_vars_native.version())
+
+build_mod(cls=Library, commands=[update_variable])
