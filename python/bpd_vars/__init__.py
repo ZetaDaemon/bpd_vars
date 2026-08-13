7424dbd9443ce2bec76575743dd2e532473ba8b3
ZetaDaemon <zetademon@icloud.com>
2026-08-14 09:01:50 +1000

    expose math_operators in bpd_vars

--- python/bpd_vars/__init__.py
+++ python/bpd_vars/__init__.py
@@ -1,9 +1,10 @@
 from mods_base import Library, build_mod
 
+from . import math_operators
 from .behavior_variable import BehaviorVariable
 from .commands import print_variables, set_variable
 
-__all__ = ["BehaviorVariable"]
+__all__ = ["BehaviorVariable", "math_operators"]
 
 
 build_mod(cls=Library, commands=[set_variable, print_variables])
